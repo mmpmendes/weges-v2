45a369ba35ac96be05ede837aff34bb1947ffc1f
Márcio Mendes <mmpmendes@users.noreply.github.com>
2025-04-27 22:09:41 +0000

    deploy e

--- ApiService/Controllers/ColaboradorController.cs
+++ ApiService/Controllers/ColaboradorController.cs
@@ -25,7 +25,7 @@ public class ColaboradorController(
     [HttpGet()]
     public IResult Get()
     {
-        var badjouras = "só para testar again 2";
+        var badjouras = "só para testar again 3";
         return Results.Ok(_mapper.Map<IEnumerable<ColaboradorDTO>>(_colaboradorRepo.GetAll()));
     }
     /// <summary>
